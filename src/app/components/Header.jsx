b64b11c0c3ea85b81b4bd118dd9e74d9db0143f5
somuraj07 <somasankar33@gmail.com>
2024-12-13 14:40:28 +0530

    auth

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,7 @@
 import { FaSearch } from "react-icons/fa";
 import Link from 'next/link'
 import React from 'react'
+import { SignedIn,SignedOut ,UserButton} from "@clerk/nextjs";
 
 export default function Header() {
   return (
@@ -28,9 +29,13 @@ export default function Header() {
 <Link href='/about'> 
 <li className="hidden md:inline text-slate-700 hover:underline">About</li>
 </Link>
-<Link href='/sign-in'> 
+<SignedIn> <UserButton /></SignedIn>
+<SignedOut>
+    <Link href='/sign-in'> 
 <li className="hidden md:inline text-slate-700 hover:underline">Signin</li>
 </Link>
+</SignedOut>
+
 </ul>
  </div>
 </header>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +20,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -27,5 +29,7 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
