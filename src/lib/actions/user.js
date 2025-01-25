a9f0fac2f851d8826479783af795fe42b4995550
somuraj07 <somasankar33@gmail.com>
2025-01-25 15:51:39 +0530

    minor changes typo

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,42 +1,38 @@
-import { use } from "react";
 import User from "../models/user.model";
- import { connect } from '../mongodb/mongoose';
+import { connect } from '../mongodb/mongoose';
 
-
- export const createOrUpdateUser = async (
-    id,
-    first_name,
-    last_name,
-    image_url,
-    email_adresses
- ) => {
-    try{
-        await connect();
-        const user = await User.findOneAndUpdate(
-         { clerkId :id },
+export const createOrUpdateUser = async (
+   id,
+   first_name,
+   last_name,
+   image_url,
+   email_adresses
+) => {
+   try {
+      await connect();
+      const user = await User.findOneAndUpdate(
+         { clerkId: id },
          {
-            $set:{
-               firstName:first_name,
-               lastName : last_name,
-               profilePicture : image_url,
-               email: email_adresses[0].email_adresses,
+            $set: {
+               firstName: first_name,
+               lastName: last_name,
+               profilePicture: image_url,
+               email: email_adresses[0].email_address,
             }
          },
-         { upsert:true, new:true}
-        );
-        return user;
+         { upsert: true, new: true }
+      );
+      return user;
+   } catch (error) {
+      console.log("error: could not create or update user", error);
+   }
+};
 
-    }catch(error){
-      console.log("error :could not create or update user",error);
-    }
-   };
-
-   export const deleteUser = async (id) => {
-      try {
-         await connect();
-         await User.findOneAndDelete({ clerkId : Id});
-      } catch (error) {
-         console.log("error : couldnt not delete the user ",error)
-      }
-   };
- 
+export const deleteUser = async (id) => {
+   try {
+      await connect();
+      await User.findOneAndDelete({ clerkId: id });
+   } catch (error) {
+      console.log("error: could not delete the user", error);
+   }
+};
